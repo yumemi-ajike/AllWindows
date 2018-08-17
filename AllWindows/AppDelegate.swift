ca28575a8b6ebbeb60e6ca117cf6bd7b252a8443
Atsushi Jike <a_jike@yumemi.co.jp>
2018-08-17 15:27:19 +0900

    Initial Commit

---
+++ AllWindows/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AllWindows
+//
+//  Created by 寺家 篤史 on 2018/08/17.
+//  Copyright © 2018年 Yumemi Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
